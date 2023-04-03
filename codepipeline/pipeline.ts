47f4dcf4e81b003bf76264af175bce8aff5d7863
Andrey Kononykhin <andkononykhin@gmail.com>
2023-04-03 15:12:32 +0300

    debug build stage
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- codepipeline/pipeline.ts
+++ codepipeline/pipeline.ts
@@ -42,14 +42,15 @@ export class MyLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
 
+      /*
       new Function(this, 'LambdaFunction', {
         runtime: Runtime.NODEJS_12_X,
         handler: 'index.handler',
         code: new InlineCode('exports.handler = _ => "Hello, CDK";')
       });
+      */
     }
 }
-
 
 export class MyPipelineAppStage extends cdk.Stage {
 
@@ -82,7 +83,14 @@ export class MyPipelineStack extends cdk.Stack {
       })
     });
 
-    pipeline.addStage(new MyPipelineAppStage(this, "test", {}));
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {}), {
+        pre: [
+            new ShellStep('validate', {
+              input: source,
+              commands: ['find .']
+            })
+        ]
+    });
   }
 }
 
@@ -90,61 +98,3 @@ const app = new cdk.App();
 new MyPipelineStack(app, 'MyPipelineStack', {});
 
 app.synth();
-
-
-/*
-
-import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import { MyPipelineAppStage } from './my-pipeline-app-stage';
-
-export class MyPipelineStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
-    });
-
-    pipeline.addStage(new MyPipelineAppStage(this, "test", {
-      env: { account: "111111111111", region: "eu-west-1" }
-    }));
-  }
-}
-
-*/
-
-/*
-
-// import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-
-const testingStage = pipeline.addStage(new MyPipelineAppStage(this, 'testing', {
-  env: { account: '111111111111', region: 'eu-west-1' }
-}));
-
-    testingStage.addPost(new ManualApprovalStep('approval'));
-
-const wave = pipeline.addWave('wave');
-wave.addStage(new MyApplicationStage(this, 'MyAppEU', {
-  env: { account: '111111111111', region: 'eu-west-1' }
-}));
-wave.addStage(new MyApplicationStage(this, 'MyAppUS', {
-  env: { account: '111111111111', region: 'us-west-1' }
-}));
-
-*/
-
-/*
-
-// self-testing
-
-stage.addPost(new ShellStep("validate", {
-  commands: ['../tests/validate.sh'],
-}));
-
-*/
-
